4b5e7e8b16386e76be82df2adfdbe78740377bf9
Marroquin02 <00025221@uca.edu.sv>
2023-11-19 22:14:55 -0600

    Add

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,19 +1,23 @@
 var express = require("express");
 require("dotenv").config();
-//const { login } = require("./database/login");
-const cors = require("cors")({ origin: true });
+const cors = require("cors");
 
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors);
+
+// Configura CORS para permitir solicitudes desde localhost y tu dominio específico
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true,
+  })
+);
 
 app.options("*", (req, res) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://xcb8vcqb-5173.use2.devtunnels.ms"
-  );
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
